411b61fad289e9b5b15384362b8444dedb9d1204
ppaint27 <84406560+ppaint27@users.noreply.github.com>
2023-03-26 01:41:28 +0700

    Initial Commit

---
+++ cosci-activity-kpi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cosci-activity-kpi
+//
+//  Created by Parisut Supchokpool on 26/3/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
